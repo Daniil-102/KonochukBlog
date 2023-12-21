10852f6a94bfc5537c1139c895838a7d76afc569
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-12-21 11:15:46 +0200

    commit

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -42,11 +42,7 @@ export const Registration = () => {
       const formData = new FormData()
       const file = e.target.files[0]
       formData.append('image', file)
-      const { data } = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, formData, {
-        headers: {
-          'Authorization': window.localStorage.getItem('token'),
-        },
-      });
+      const { data } = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, formData);
       setImageUrl(data.url)
     } catch (err) {
       console.log(err);
