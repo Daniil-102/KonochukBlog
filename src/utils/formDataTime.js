a29c3b762b93d2db70e5cfd827c16558dc0b6980
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2024-02-14 12:41:27 +0200

    c

--- src/utils/formDataTime.js
+++ src/utils/formDataTime.js
@@ -1,5 +1,5 @@
 export function formatDateTime(dateTimeString) {
-  const date = new Date(dateTimeString.toString());
+  const date = new Date(dateTimeString);
 
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
